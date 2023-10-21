59e43f6fb2ebf3a7def0a042f3afdb5c04d3df53
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-10-21 14:59:59 -0400

    commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # hackerrank
+*/ The code in this repository is a JavaScript implementation of a left rotation function for an array. It takes an array and the number of positions to rotate as input, and it returns a new array with the specified left rotation applied. The corrected code ensures that the rotation is done correctly. /*
---
+++ left-rotation-operation.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function rotateLeft(d, arr) {
+    const n = arr.length;
+    const rotatedArr = [];
+
+    const startIndex = d % n;
+
+    for (let i = startIndex; i < n; i++) {
+        rotatedArr.push(arr[i]);
+    }
+
+    for (let i = 0; i < startIndex; i++) {
+        rotatedArr.push(arr[i]);
+    }
+
+    return rotatedArr;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+    const d = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = rotateLeft(d, arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
